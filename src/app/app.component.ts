49f0d7bca86f0df1b39a97e755c212b0b96ba655
William Sánchez <will~.1996.ws23@gmail.com>
2024-09-26 21:19:46 -0500

    primer commit del proyecto de angular~

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFirstApp';
+  saludo = 'Hello Bebukis'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Empleadoscomponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, Empleadoscomponent, EmpleadoComponent
   ],
   imports: [
     BrowserModule,
